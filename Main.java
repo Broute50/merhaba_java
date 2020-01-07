54e29f29efe6c17618b69015aa464cfab15c9ac3
Broute50 <48528044+Broute50@users.noreply.github.com>
2020-01-07 16:25:47 +0300

    java project just code

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    //reusability tekrar tekrar kullanılabilirlik. Bir kere yaz her yerde kullan...
+    public static void main(String[] args) {
+
+        // System.out.println("Merhaba Java");
+
+        //int ogrenciSayisi = 10; // camel case mantığı ile kullanılıyor. İlk harf küçük sonraki harf büyük oluyor
+        //String mesaj = "Öğrenci Sayısı :";
+        //System.out.println(mesaj + ogrenciSayisi);
+
+    }
+}
